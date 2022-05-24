b3754b77f995cf79dba91f726eca3c693ca0261c
Antonio Henriques de França Terceiro <antoniomed1.an@gmail.com>
2022-05-23 21:08:06 -0300

    adiciona firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy, IonInput, IonLabel } from '@ionic/angular';
+import {
+  IonicModule,
+  IonicRouteStrategy,
+  IonInput,
+  IonLabel,
+} from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +17,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ModalPopoverPageRoutingModule } from './pages/modal-popover/modal-popover-routing.module';
 import { NgxMaskModule, MaskPipe } from 'ngx-mask';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from './../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -26,9 +37,16 @@ import { NgxMaskModule, MaskPipe } from 'ngx-mask';
     IonicModule,
     ModalPopoverPageRoutingModule,
     NgxMaskModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFirestoreModule,
+    AngularFireDatabaseModule,
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    MaskPipe],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    MaskPipe,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
